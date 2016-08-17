e36fe57b11ea345b6e3a62bdd33e69aac8a9cd39
Will Xu <wxu@WillXu-Horton.local>
2016-08-17 14:29:06 -0700

    Fix sales fact table column types

--- sql/table_fact_sales.sql
+++ sql/table_fact_sales.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS corp.fact_sales (
-    product_id char(20) NOT NULL,
+    product_id INTEGER NOT NULL,
     time_id char(20) NOT NULL,
-    customer_id char(20) NOT NULL,
+    customer_id INTEGER NOT NULL,
     promotion_id char(20),
-    store_id char(20) NOT NULL,
+    store_id INTEGER NOT NULL,
     store_sales FLOAT,
     store_cost FLOAT,
     unit_sales FLOAT,
